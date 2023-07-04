cd13744766d4f59b47d6b7cdbeecc77ef285dfc6
Ishani Malviya <86361355+ishhanayy@users.noreply.github.com>
2023-07-04 16:49:11 +0530

    Basic Binary Prob
    Of Balanced Binary Tree

---
+++ Tree/7_BalancedBinaryTree.cpp
@@ -0,0 +1,54 @@
+//PROBLEM STATEMENT : https://leetcode.com/problems/balanced-binary-tree/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    int helper(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int h1 = helper(root->left);
+        if(h1==-1)
+        {
+            return -1;
+        }
+        int h2 = helper(root->right);
+        if(h2==-1)
+        {
+            return -1;
+        }
+        if(abs(h1-h2)>1)
+        {
+            return -1;
+        }
+        return max(h1, h2)+1;
+    }
+    bool isBalanced(TreeNode* root) 
+    {
+        //Balanced Binary Tree : If in any Tree height(left)-height(right)<=1 then it is Balanced Binary Tree.
+        int ans = helper(root); 
+        if(ans==-1)
+        {
+            return false;
+        }   
+        else 
+        {
+            return true;
+        }
+    }
+};
