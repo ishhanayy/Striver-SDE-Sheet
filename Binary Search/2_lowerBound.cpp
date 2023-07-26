247cd3fd06c8a48680d8c17c5280e2f0150f3dfe
Ishani Malviya <86361355+ishhanayy@users.noreply.github.com>
2023-07-26 19:14:18 +0530

    Lower Bound : The smallest index such that arr[i]≥target , such that if the arr = [2, 3, 4, 5, 8, 10] and the target is 5, then the lower bound would be i = 3.

---
+++ Binary Search/2_lowerBound.cpp
@@ -0,0 +1,24 @@
+//PROBLEM STATEMENT : https://leetcode.com/problems/binary-search/
+
+/*----------------------ITERATIVE---------------------------*/
+int lowerBound(vector<int> arr, int n, int tar) 
+{
+	int low =0;
+	int high = n-1;
+	int ans = n; //atleast the answer will always be hypothetical lower bound, 
+	while(low<=high)
+	{
+		int mid = low+(high-low)/2;
+		//we have to find the lower bound, and in the binary search instead of finding the element we are searching for the linear bound index
+		if(arr[mid]>=tar)
+		{
+			high = mid-1;
+			ans = mid; // we have to find the index of the lower bound
+		}
+		else
+		{
+			low = mid+1;
+		}
+	}
+	return ans;
+}
