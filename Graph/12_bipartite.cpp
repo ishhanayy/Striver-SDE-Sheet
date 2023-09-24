6cc5e8b01b09efd7b40163b609d9ba4405d09f0d
Ishani Malviya <86361355+ishhanayy@users.noreply.github.com>
2023-09-24 15:41:40 +0530

    Update 12_bipartite.cpp

--- Graph/12_bipartite.cpp
+++ Graph/12_bipartite.cpp
@@ -47,3 +47,82 @@ public:
         return true;
     }
 };
+
+//**************************************************************************************
+
+//PROBLEM STATMENT : https://practice.geeksforgeeks.org/problems/bipartite-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bipartite-graph
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+private:
+    bool check(int start, int v, vector<int> adj[], vector<int>& colour)
+    {
+        queue<int> q;
+	    q.push(start);
+	    colour[start] = 0;
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        for(auto it : adj[node])
+	        {
+	            if(colour[it]==-1)
+	            {
+	                q.push(it);
+	                colour[it] = !colour[node];
+	            }
+	            else if(colour[it]==colour[node])
+	            {
+	                return false;
+	            }
+	        }
+	    }
+	    
+	    return true;
+    }
+public:
+	bool isBipartite(int v, vector<int>adj[]){
+	    vector<int> colour(v, -1);
+	    //we will take 0 and 1 as the color 
+	    for(int i = 0; i<v; i++)
+	    {
+	        if(colour[i]==-1)
+	        {
+	            if(check(i, v, adj, colour)==false)
+	            {
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+	
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isBipartite(V, adj);    
+		if(ans)cout << "1\n";
+		else cout << "0\n";  
+	}
+	return 0;
+}
+// } Driver Code Ends
