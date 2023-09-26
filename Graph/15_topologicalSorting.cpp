5ae4edd0deadd82f37717f1f56dfe38e5c003cb4
Ishani Malviya <86361355+ishhanayy@users.noreply.github.com>
2023-09-26 17:33:33 +0530

    Update 15_topologicalSorting.cpp
    added BFS code for the problem which is commonly known as Kahn's algorithm.

--- Graph/15_topologicalSorting.cpp
+++ Graph/15_topologicalSorting.cpp
@@ -11,6 +11,7 @@
 
 //PROBLEM STATEMENT : https://practice.geeksforgeeks.org/problems/topological-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=topological-sort
 
+//Using DFS
 
 //{ Driver Code Starts
 #include <bits/stdc++.h>
@@ -56,6 +57,106 @@ class Solution
 	    return ans;
 	}
 	
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
+
+//Using BFS
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int v, vector<int> adj[]) 
+	{
+	    vector<int> inDegree(v, 0);
+	    for(int i = 0; i<v; i++)
+	    {
+	        for(auto it : adj[i])
+	        {
+	            inDegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i = 0; i<v; i++)
+	    {
+	        if(inDegree[i]==0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int> ans;
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        ans.push_back(node);
+	        q.pop();
+	        for(auto it : adj[node])
+	        {
+	            inDegree[it]--;
+	            if(inDegree[it]==0)
+	            {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    return ans;
+	}
 };
 
 //{ Driver Code Starts.
